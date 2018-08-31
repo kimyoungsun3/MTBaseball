63c6c55c3df2fd4ce01e29c957c1a7f3f43baf15
kimyoungsun3 <40338139+kimyoungsun3@users.noreply.github.com>
2018-08-31 13:54:28 +0900

    222
    22

---
+++ K6_03_SQL/00_10_02(Fun)GetLevel.sql
@@ -0,0 +1,37 @@
+﻿/*
+-----------------------------------------------------
+--		  		       lv <= exp
+select dbo.fnu_GetLevel(       -1 ) -- 1
+select dbo.fnu_GetLevel(        0 ) -- 1
+select dbo.fnu_GetLevel(     4000 ) -- 1
+select dbo.fnu_GetLevel(     4001 ) -- 2
+select dbo.fnu_GetLevel(     8200 ) -- 2
+select dbo.fnu_GetLevel(     8201 ) -- 3
+select dbo.fnu_GetLevel( 44651200 ) -- 649
+select dbo.fnu_GetLevel( 44651201 ) -- 650
+select dbo.fnu_GetLevel( 99999999 ) -- 650
+-----------------------------------------------------
+*/
+use GameMTBaseball
+GO
+
+IF OBJECT_ID ( N'dbo.fnu_GetLevel', N'FN' ) IS NOT NULL
+	DROP FUNCTION dbo.fnu_GetLevel;
+GO
+
+CREATE FUNCTION dbo.fnu_GetLevel(
+	@exp_  			int
+)
+	RETURNS int
+AS
+BEGIN
+	declare @rtn 		int	set @rtn 		= 0
+	select @rtn = (Sqrt ((@exp_ - 1) / 100 + 380.25) - 19.5) + 1;
+	set @rtn = case 
+					when @rtn <=   0 then 	1
+					when @rtn >  650 then 	650
+					else @rtn
+				end
+	
+	RETURN @rtn
+END
---
+++ K6_03_SQL/00_61(Fun)시간차.sql
@@ -0,0 +1,53 @@
+﻿/*
+-----------------------------------------------------
+select dbo.fnu_GetDatePart('mi', '2013-11-26 12:30:00', '2013-11-26 13:30:00')
+
+select dbo.fnu_GetDatePart('dd', '2013-11-26',          '2013-11-26'         )
+select dbo.fnu_GetDatePart('dd', '2013-11-26',          '2013-11-27'         )
+select dbo.fnu_GetDatePart('dd', '2013-11-26',          '2013-12-06'         )
+select dbo.fnu_GetDatePart('dd', '2013-11-26',          '2013-12-27'         )
+
+
+select dbo.fnu_GetDatePart('hh', '2013-11-26 12:30:00', '2013-11-27 13:30:00')
+select dbo.fnu_GetDatePart('hh', '2013-11-26 12:30:00', '2013-11-28 13:30:00')
+
+select dbo.fnu_GetDatePart('hh', '2013-11-26 12:30:00', '2013-11-26 13:29:00')
+select dbo.fnu_GetDatePart('hh', '2013-11-26 12:30:00', '2013-11-26 13:30:00')
+select dbo.fnu_GetDatePart('hh', '2013-11-26 12:30:00', '2013-11-26 13:31:00')
+select dbo.fnu_GetDatePart('hh', '2013-11-26 12:30:00', '2013-11-26 14:31:00')
+select dbo.fnu_GetDatePart('hh', '2013-11-26 12:30:00', '2013-11-27 12:30:00')
+select dbo.fnu_GetDatePart('hh', '2013-11-26 12:30:00', '2013-11-27 13:30:00')
+-----------------------------------------------------
+*/
+use Game4FarmVill3
+GO
+
+IF OBJECT_ID ( N'dbo.fnu_GetDatePart', N'FN' ) IS NOT NULL
+	DROP FUNCTION dbo.fnu_GetDatePart;
+GO
+
+CREATE FUNCTION dbo.fnu_GetDatePart(
+	@kind_					varchar(2),
+
+	@startDate_  			datetime,
+	@endDate_  				datetime
+)
+	RETURNS int
+AS
+BEGIN
+	declare @rtn 			int				set @rtn 		= 0
+	declare @datedif		datetime
+
+
+	set @rtn = DATEDIFF (s, @startDate_, @endDate_)
+
+	set @rtn = case
+					when @kind_ = 'dd' then 	@rtn/(60*60*24)
+					when @kind_ = 'hh' then 	@rtn/3600
+					when @kind_ = 'mi' then 	@rtn/60
+					else 						@rtn
+				end
+
+	RETURN @rtn
+
+END
